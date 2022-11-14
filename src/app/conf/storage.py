87a098f94b47b1a2d0fad89897edcc611b05bc22
GLEF1X <glebgar567@gmail.com>
2022-11-14 12:11:31 -0500

    [experimental]: make media files publicly accessible

--- src/app/conf/storage.py
+++ src/app/conf/storage.py
@@ -11,5 +11,6 @@ if not settings.DEBUG:
     AWS_S3_REGION_NAME = env('S3_REGION_NAME')
     AWS_STORAGE_BUCKET_NAME = env('S3_BUCKET_NAME')
     AWS_S3_ENDPOINT_URL = f"https://glefixmedia.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
+    AWS_DEFAULT_ACL = env('S3_DEFAULT_ACL', default='public-read')
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
