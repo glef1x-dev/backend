d8e2cfeba9da2d24d47af244b5550bb6e8b10201
GLEF1X <glebgar567@gmail.com>
2022-11-21 14:08:48 -0500

    [deployment]: add more settings for `django-storages` S3

--- src/app/conf/storage.py
+++ src/app/conf/storage.py
@@ -13,5 +13,7 @@ if not settings.DEBUG:
     AWS_S3_ENDPOINT_URL = f"https://glefixmedia.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
     AWS_DEFAULT_ACL = env('S3_DEFAULT_ACL', default='public-read')
     AWS_S3_CUSTOM_DOMAIN = env('S3_CUSTOM_DOMAIN')
+    AWS_IS_GZIPPED = env('S3_IS_GZIPPED', default=False)
+    AWS_QUERYSTRING_AUTH = env('S3_QUERYSTRING_AUTH', default=True)
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
