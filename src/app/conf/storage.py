b80e63044f8095cadd000f436799182fe3445b3b
GLEF1X <glebgar567@gmail.com>
2022-11-21 14:06:04 -0500

    [deployment]: add `AWS_S3_CUSTOM_DOMAIN` setting to change cdn domain

--- src/app/conf/storage.py
+++ src/app/conf/storage.py
@@ -12,5 +12,6 @@ if not settings.DEBUG:
     AWS_STORAGE_BUCKET_NAME = env('S3_BUCKET_NAME')
     AWS_S3_ENDPOINT_URL = f"https://glefixmedia.{AWS_S3_REGION_NAME}.digitaloceanspaces.com"
     AWS_DEFAULT_ACL = env('S3_DEFAULT_ACL', default='public-read')
+    AWS_S3_CUSTOM_DOMAIN = env('S3_CUSTOM_DOMAIN')
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
