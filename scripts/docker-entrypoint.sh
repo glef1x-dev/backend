d5eb525f4b28074fba989372886076d7244a4266
GLEF1X <glebgar567@gmail.com>
2022-12-03 15:55:40 -0500

    [deploy]: add `--noinput` flag for collectstatic and migration

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 # activate our virtual environment here
 . "$VENV_PATH"/bin/activate
 
-cd src && ./manage.py migrate && ./manage.py collectstatic -i "admin/*" -i "baton/*"
+cd src && ./manage.py migrate && ./manage.py collectstatic --noinput -i "admin/*" -i "baton/*"
 
 gunicorn --reload --chdir /app/src -b :8000 --access-logfile - app.wsgi:application
