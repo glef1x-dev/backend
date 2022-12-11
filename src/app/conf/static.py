ba790785862763d34a24bdd58345f11aa5b25a8a
GLEF1X <glebgar567@gmail.com>
2022-12-11 16:35:18 -0500

    [bugfix]: `STATICFILES_STORAGE` must be defined only in production

--- src/app/conf/static.py
+++ src/app/conf/static.py
@@ -4,9 +4,9 @@ from app.conf.env_reader import env
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
 STATIC_ROOT = env("STATIC_ROOT", cast=str, default="static")
 
 if settings.DEBUG:
     STATIC_URL = "/static/"
+else:
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
