99b5b708678b0b3258b8b116429974a356a99ae2
GLEF1X <glebgar567@gmail.com>
2022-12-23 16:08:39 -0500

    [bugfixes]: change logic of setting refresh token cookie

--- src/a12n/utils.py
+++ src/a12n/utils.py
@@ -1,15 +1,18 @@
-import functools
-from typing import Any
-
 from rest_framework.response import Response
 from rest_framework_simplejwt.settings import api_settings
 
 from django.conf import settings
 
+_BASE_COOKIES_PARAMS = {
+    "key": settings.REFRESH_TOKEN_COOKIE_NAME,
+    "path": "/",
+    "samesite": "Lax",
+}
+
 
 def set_refresh_token_cookie(response: Response) -> Response:
     refresh_token = response.data["refresh"]
-    set_cookie_params = _get_base_cookie_params() | {
+    set_cookie_params = _BASE_COOKIES_PARAMS | {
         "value": refresh_token,
         "max_age": api_settings.REFRESH_TOKEN_LIFETIME,
         "httponly": True,
@@ -20,21 +23,4 @@ def set_refresh_token_cookie(response: Response) -> Response:
 
 
 def delete_refresh_token_cookie(response: Response) -> None:
-    response.delete_cookie(**_get_base_cookie_params())
-
-
-@functools.lru_cache
-def _get_base_cookie_params() -> dict[str, Any]:
-    base_cookie_params = {
-        "key": settings.REFRESH_TOKEN_COOKIE_NAME,
-        "path": "/",
-        "samesite": "None",
-    }
-
-    if settings.DEBUG:
-        return base_cookie_params
-
-    base_cookie_params.update(
-        domain=f"https://{settings.ROOT_DOMAIN}",
-    )
-    return base_cookie_params
+    response.delete_cookie(**_BASE_COOKIES_PARAMS)
