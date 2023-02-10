e285b1e192a1c5fbd035e6d2d998185f55236c74
GLEF1X <glebgar567@gmail.com>
2023-02-10 16:59:24 -0500

    [image processing]: upgrade quality of processed images

--- src/common/images.py
+++ src/common/images.py
@@ -9,6 +9,6 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 
 def convert_image_to_webp_format(image: SimpleUploadedFile) -> None:
     # TODO: add some check if the SimpleUploadedFile is really an image
-    Image.open(image).save(image, "webp", optimize=True, quality=60)
+    Image.open(image).save(image, "webp", optimize=True, quality=90)
     image.name = pathlib.Path(image.name).with_suffix(".webp")
     image.content_type = "image/webp"
