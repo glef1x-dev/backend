98d8eff6068911257acba030e8fc45be018dcfd8
GLEF1X <glebgar567@gmail.com>
2023-03-01 14:01:01 -0500

    [refactoring]: add more data in the response

--- src/third_party/api/viewsets.py
+++ src/third_party/api/viewsets.py
@@ -17,6 +17,10 @@ class GithubApiView(GenericAPIView):
     def get(self, request: Request, repo: str, owner: str) -> Response:
         repository = self.github_service.get_repository(repo, owner)
         return Response(
-            {"stargazers_count": repository.stargazers_count},
+            {
+                "stargazers_count": repository.stargazers_count,
+                "html_url": repository.html_url,
+                "full_name": repository.full_name,
+            },
             content_type="application/json",
         )
